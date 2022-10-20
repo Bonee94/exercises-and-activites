acacd0b13088f261763998d6b3d11f5276c1afb8
Bonee94 <bonee94@yahoo.com>
2022-10-19 22:48:15 -0500

    JS Activity Completed

---
+++ Js Variables/script.js
@@ -0,0 +1,17 @@
+// Write Your JavaScript Code Here
+var personName = 'Taylor';
+var pets = '6';
+var funFact = 'I own a skoolie.'
+
+
+console.log('My name is ' + (personName));
+console.log('I have ' + (pets) + ' wondeerful pets.')
+console.log('Here is a fun fact, ' + (funFact))
+
+personName = 'Neil';
+funFact = 'I love cheese!'
+
+console.log('Work Partner:')
+console.log('My name is ' + (personName))
+console.log('I have ' + (pets) + ' wondeerful pets.')
+console.log('Here is a fun fact, ' + (funFact))
