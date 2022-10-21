f5e4e798caf18ee3b3a9c791aebf47fa2144d9c6
Bonee94 <bonee94@yahoo.com>
2022-10-20 21:05:32 -0500

    Scope activity completed

--- Scope/script.js
+++ Scope/script.js
@@ -1,4 +1,6 @@
 // TODO: Declare variable 'shout' with the value 'Shout' so it's available to `justShout` and `shoutItAllOut` functions
+var shout = 'Shout';
+
 function justShout() {
   console.log(shout + ", " + shout);
   return;
@@ -10,6 +12,7 @@ function shoutItAllOut() {
 }
 
 justShout();
+
 shoutItAllOut();
 
 // TODO: Declare variable 'animal' with the value 'Tigers' so it is only available to the 'sayTigers' function
@@ -20,15 +23,16 @@ function sayLions() {
 }
 
 function sayTigers() {
+  var animal = 'Tigers';
   console.log("and " + animal + " and "); 
   return;
 }
 
 // TODO: The variable 'bears' should only declared once and 'sayBears' should return "Bears! OH MY!". 
-var bears = "Bears";
+
 
 function sayBears() {
-    var bears = "Pandas";
+    var bears = "Bears";
     console.log(bears + "! OH MY!");
     return;
 }
@@ -37,10 +41,10 @@ sayLions();
 sayTigers();
 sayBears();
 
-// TODO: The variable 'sing' should be declared once in the local scope.
-var sing = "Sing"; 
+// TODO: The variable 'sing' should be declared once in the local scope. 
 
 function singAlong() {
+  var sing = "Sing";
   console.log(sing + ",");
   var singASong = function () {
     console.log(sing + " a Song.");
