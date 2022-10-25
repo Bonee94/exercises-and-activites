065b43e2c1800384dbd88c30e5ad79475a9781e9
Bonee94 <bonee94@yahoo.com>
2022-10-25 18:58:27 -0500

    Activity completed

---
+++ Window Object/script.js
@@ -0,0 +1,16 @@
+// TODO: What will the following line of code log?
+// It will log every property in the current window
+console.log(window);
+
+// TODO: What will the following line of code log?
+//This will log what is withing our HTML Document.
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+// This will log the Documents as an element object. For us it would be an html.
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+// This will log the Head element
+console.log(document.head);
+
