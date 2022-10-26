f3759a5f0a9636913a22f9e787208af09f513682
Bonee94 <bonee94@yahoo.com>
2022-10-25 19:30:53 -0500

    Activity completed

---
+++ Traverse Dom/assets/js/script.js
@@ -0,0 +1,7 @@
+// Access element using id
+var headerDiv = document.getElementById('header');
+var articlesDiv = document.getElementById('articles');
+
+// Change style by accessing style object's properties
+headerDiv.children[0].style.color = 'white';
+articlesDiv.children[0.0].style.fontSize = '50px';
